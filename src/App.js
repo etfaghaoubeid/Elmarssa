e6c256c54f7d2b91d5c5c131638e186b9eefafb5
etfaghaoubeid <etfaghaoubeid@gmail.com>
2020-03-11 09:21:47 +0000

    install react-router-dom and add withRouter to menu-item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import {Route} from "react-router-dom"
 import "./App.css"
 import HomePage from './pages/homepage/HomePage.component';
+
+const HatPage = ()=>(
+  <h1>HatPage</h1>
+)
 
 function App() {
   return (
     <div>
-    <HomePage/>
+      <Route exact path="/" component={HomePage}/>
+      <Route path="/shop/hats" component={HatPage} />
     </div>
   );
 }

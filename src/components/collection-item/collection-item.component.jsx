4418130dd7221d3993e8fe71ee6c3613eabe3113
etfaghaoubeid <etfaghaoubeid@gmail.com>
2020-03-12 11:34:28 +0000

    fix some styling issues

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import "./collection-item.styles.scss"
 
 const CollectionItem = ({imageUrl , name , price})=>(
-   <div className="collection-item">
-       <div style={{backgroundImage:`url(${imageUrl})`}} className="image"/>
-          
-       <div className="collection-footer">
-         <span className="name">{name}</span>
-          <span className="price">{price}</span>
-       </div>
+   <div className='collection-item'>
+   <div
+     className='image'
+     style={{
+       backgroundImage: `url(${imageUrl})`
+     }}
+   />
+   <div className='collection-footer'>
+     <span className='name'>{name}</span>
+     <span className='price'>{price}</span>
    </div>
-)
+ </div>
+) 
 
 export default CollectionItem ;
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,13 +2,16 @@ import React from "react"
 import "./collection-preview.styles.scss";
 import CollectionItem from "../collection-item/collection-item.component";
 const CollectionPreview = ({title,  items} )=>(
-    <div className="collection-preview">
-     <h1 className="title">{title.toUpperCase()}</h1>
-    <div className="preview">
-      {items.filter((item, indx)=>indx<4).map(({id , ...otheItemProps})=>(<CollectionItem key={id} {...otheItemProps}/>))}
-    
-      </div>
-    </div>    
+  <div className='collection-preview'>
+  <h1 className='title'>{title.toUpperCase()}</h1>
+  <div className='preview'>
+    {items
+      .filter((item, idx) => idx < 4)
+      .map(({ id, ...otherItemProps }) => (
+        <CollectionItem key={id} {...otherItemProps} />
+      ))}
+  </div>
+</div>
 )
 
 export default CollectionPreview; 

780fb4caf4f056e028aa491d486a01ae6f487b84
etfagha oubeid <etfagha@softprofiles.com>
2020-05-14 16:31:45 +0000

    clean directory component

--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { connect } from "react-redux";
+import {createStructuredSelector} from "reselect"
 
 import MenuItem from "../menu-item/menu-item.component"
 
 import { DirectoryContainer } from "./directory.styles"
 
-import {createStructuredSelector} from "reselect"
 import {selectDirectorySections} from "../../redux/directory/directory.selectors"
 
 const Directory =({sections,})=> {
